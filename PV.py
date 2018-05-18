6274553edf684ca7ff1ec4f1d81c92e744d5b1d2
mkaffash <mahtab.kaffash69@gmail.com>
2018-05-18 11:25:49 +0200

    fisrt version of PV simulator

--- PV.py
+++ PV.py
@@ -4,7 +4,6 @@ class PV():
         self.efficiency = efficiency
 
     def PV_generation(self):
+        E_PV_in = self.solar * self.efficiency
         return E_PV_in()
 
-    def E_PV_in(self):
-        return self.solar * self.efficiency
