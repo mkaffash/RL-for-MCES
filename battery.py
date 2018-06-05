d090821ddd4174667772d27ef2340a7834e8822d
mkaffash <mahtab.kaffash69@gmail.com>
2018-06-05 13:46:29 +0200

    edited version (major changes in HP)

--- battery.py
+++ battery.py
@@ -1,67 +1,50 @@
-'''
-Created on 15 apr. 2018
+import pandas as pd
+import numpy as np
 
-@author: MBUWIRB
-'''
 class Battery():
-    DISCHARGE = -1
-    CHARGE = 1
-    IDLE = 0
+    def __init__(self, charge_efficiency, bat_capacity, discharging_rate,charging_rate, horizon = 24):
+        self.charge_efficiency = charge_efficiency
+        self.discharge_efficiency = 1
+        self.bat_capacity = bat_capacity #in kWh
+        self.charge_rate = charging_rate #in kW
+        self.discharge_rate = discharging_rate
+        self.time_delta = 1 # simulation timestep of one hour
+        self.horizon = horizon
+        self.max_SoC = 1
+        self.min_SoC = 0.2
+        self.compute_energy_cost = False #calculates the cost of buying energy to charge battery or  money recieved for selling enrgy from discharging the battery
 
-    def __init__(self, capacity, charging_rate):
-        self.capacity           = capacity          # Energy capacity in Wh
-        self.charging_rate      = charging_rate           # Power in W
-        self.efficiency         = 0.95
-        self.deltaT             = 60*60             # timestamp in seconds for hourly data
+    def do_action (self, data, compute_energy_cost): #data = dataframe with information on the SoC of the battery
+        # data['action'] = [action]
+        self.compute_energy_cost = compute_energy_cost
+        data['delta_energy'] = np.where(data['initial_action'] > 0 , (self.charge_efficiency*self.charge_rate * self.time_delta)/self.bat_capacity, (self.discharge_rate * self.time_delta)/self.bat_capacity)
+        data['delta_energy'] = np.where(data['initial_action'] == 0 , 0, data['delta_energy'])
 
-    def reset(self):
-        self.state = {
-            "charge": 0
-        }
+        self.battery_backup_controller(min_SoC_action = 1, max_SoC_action = -1,data = data)
 
-        return self._observe()
+        data['new_SoC'] = data['SoC']  + data['action'] * data['delta_energy']
+        # data['delta_energy'] = self.delta_energy
+        if self.compute_energy_cost:
+            data['energy_cost'] =  data['delta_energy'] * data['price']
+        return data
 
-    def step(self, u):
-        E      = self.execute_action(u)
+    def reset_state(self,data):
+        data['SoC'] = [0 for _ in range(self.horizon)]
 
-        return self._observe(), u, E
+    def battery_backup_controller(self, min_SoC_action, max_SoC_action, data):
+        data['action'] = np.where(data['SoC'] > self.max_SoC, max_SoC_action, data['initial_action'])
+        data['action'] = np.where(data['SoC'] < self.min_SoC, min_SoC_action, data['initial_action'])
 
-    def execute_action(self, u):
-        # If charging, the efficiency needs to be taken into account to calculate the battery charge
-        if u == self.CHARGE or u == self.IDLE:
-            efficiency = self.efficiency
-        # When discharging, the full amount of energy will be discharged from the battery
-        elif u == self.DISCHARGE:
-            efficiency = 1
+        return data
 
-        # u should have either of the following values: 1 (charging), 0 (idle), -1 (discharging)
-        delta_charge          = u * self.charging_rate * self.deltaT
-        self.state["charge"] += delta_charge * self.efficiency / 3600
 
-        E = self.total_energy(delta_charge)
 
-        return E
+if __name__ == '__main__':
+    optimization_horizon = 3
+    action_space = [-1,0,1] #-1 = DISCHARGE, 0 = IDLE, 1 = CHARGE
+    battery = Battery(charge_efficiency = 0.95, bat_capacity = 15, discharging_rate = 1.5, charging_rate = 2, horizon = optimization_horizon)
+    data = pd.DataFrame({'SoC': np.linspace(0.1,0.5,optimization_horizon), 'price': np.linspace(1,4,optimization_horizon), 'initial_action': action_space, 'action': action_space})
 
-    def total_energy(self, delta_charge):
-        # Compute excess energy and adjust battery state.
-        if self.state["charge"] > self.capacity:
-            # Calculate the energy that the battery took from the environment
-            E = delta_charge - ((self.state["charge"] - self.capacity) / self.efficiency)
-            self.state["charge"] = self.capacity
-        elif self.state["charge"] < 0:
-            # Calculate the energy that the battery delivers to the environment
-            E = (delta_charge + self.state["charge"]) * self.efficiency
-            self.state["charge"] = 0
-        # If no excess energy, compute the amount of energy that has been delivered to or from the grid
-        else:
-            if delta_charge < 0:
-                E = delta_charge * self.efficiency
-            elif delta_charge > 0:
-                E = delta_charge / self.efficiency
-            else:
-                E = 0
-
-        return E
-
-    def _observe(self):
-        return self.state
+    # battery.battery_backup_controller(min_SoC_action = 1, max_SoC_action = -1,data = data)
+    battery.do_action(data,compute_energy_cost = False)
+    print(data)
