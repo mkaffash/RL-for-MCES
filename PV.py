932076b38461f5acc63e7a4d2be9aab13ef3ce3e
mkaffash <38406847+mkaffash@users.noreply.github.com>
2018-06-05 14:06:59 +0200

    Delete PV.py

--- PV.py
+++
@@ -1,9 +0,0 @@
-class PV():
-    def __init__(self, solar, efficiency):
-        self.solar = solar          # Energy imported to PV in Wh
-        self.efficiency = efficiency
-
-    def PV_generation(self):
-        E_PV_in = self.solar * self.efficiency
-        return E_PV_in()
-
