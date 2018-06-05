fc1b139c60d97ad5edff27e453f6dcaa5a8e5e0a
mkaffash <mahtab.kaffash69@gmail.com>
2018-06-05 13:47:49 +0200

    edited version (major changes in HP)

--- Heatpump.py
+++ Heatpump.py
@@ -1,10 +1,28 @@
-class Heatpump():
-    def __init__(self, input_energy, efficiency):
-        self.E_in_HP = input_energy          # Energy imported to HP in Wh
+import pandas as pd
+import numpy as np
+class HP():
+    def __init__(self, efficiency):
         self.efficiency = efficiency
-        self.max_out_HP = 14  # max energy output of heat pump in Wh
 
+    def get_td_data(self, file_name, day, night, bioler_output ,horizon, data):
+        space_heating = pd.read_csv(file_name)
+        day_zone = space_heating[[day]].values
+        night_zone = space_heating[[night]].values
+        thermal_demand = np.zeros(8400)
+        alpha = np.zeros(8400) #define whether HP should be on or off based on max nominal of boiler
+        for i in range(8400):
+            thermal_demand[i] = (day_zone[i] + night_zone[i]) / 1000  # convert the data from W to kW
+            if thermal_demand[i]>= bioler_output:
+                alpha[i]=1 #means HP should be on
+            else:
+                alpha[i] = 0 #heat pump should be off
+        data['HP'] = (thermal_demand - bioler_output)*alpha / self.efficiency
+        return data['HP']
 
-    def HP_output(self):
-        E_HP_out = self.E_in_HP * self.efficiency
-        return E_HP_out()
+# if __name__ == '__main__':
+#     horizon = 10
+#     HP_model = HP(efficiency=0.9)
+#     data = pd.DataFrame()
+#     print('--------')
+#     HP = HP_model.get_td_data('data-openideas.csv','out_QradD','out_QradN', 35,  horizon, data)
+#     print(np.max(data))
